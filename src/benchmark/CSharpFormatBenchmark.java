064e56569ba419b1d96e93de20153b9c9741a1f2
Armin Hueneburg <hueneburg.armin@gmail.com>
2015-07-30 18:17:56 +0200

    more extensive tests

--- src/benchmark/CSharpFormatBenchmark.java
+++ src/benchmark/CSharpFormatBenchmark.java
@@ -30,25 +30,26 @@ public class CSharpFormatBenchmark {
     }
 
     private static long custom() throws FormatException, ArgumentNullException {
-        String a = "{0} this is a test {1} {0}";
+        String a = "{4}{2}{5}{5}{1}{4}{2}{4}{4}{1}{0}{2}{3}{1}{1}{0}{3}{0}{1}{0}{1}{1}{1}{1}{1}{4}{4}{0}{2}{5}";
         long time = System.nanoTime();
-        CSharpFormat.format(a, "a", "b");
+        CSharpFormat.format(a, "a", "b", "c", "d", "e", "f");
         time -= System.nanoTime();
         return time * -1;
     }
 
     private static long format() {
-        String a = "%s this is a test %s %s";
+        String a = "%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s";
         long time = System.nanoTime();
-        String.format(a, "a", "b", "a");
+        String.format(a, "e", "c", "f", "f", "b", "e", "c", "e", "e", "b", "a", "c", "d", "b", "b", "a", "d", "a", "b",
+                "a", "b", "b", "b", "b", "b", "e", "e", "a", "c", "f");
         time -= System.nanoTime();
         return time * -1;
     }
 
     private static long replace() throws FormatException, ArgumentNullException {
-        String a = "{0} this is a test {1} {0}";
+        String a = "{4}{2}{5}{5}{1}{4}{2}{4}{4}{1}{0}{2}{3}{1}{1}{0}{3}{0}{1}{0}{1}{1}{1}{1}{1}{4}{4}{0}{2}{5}";
         long time = System.nanoTime();
-        a.replace("{0}", "a").replace("{1}", "b");
+        a.replace("{0}", "a").replace("{1}", "b").replace("{2}", "c").replace("{3}", "d").replace("{4}", "e").replace("{5}", "f");
         time -= System.nanoTime();
         return time * -1;
     }
